bc5df37bf0021aae268acbe80afe090c78521b9d
migo1 <ertmigot@gmail.com>
2023-07-25 14:27:09 +0300

    add-tailwind-render-mission

--- src/components/missions/Mission.js
+++ src/components/missions/Mission.js
@@ -1,6 +1,7 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { getMissions } from '../../redux/missions/missionsSlice';
+import MissionList from './MissionList';
 
 function Mission() {
   const missionsData = useSelector((state) => state.missions);
@@ -10,10 +11,44 @@ function Mission() {
     dispatch(getMissions());
   }, [dispatch]);
 
-  console.log(missionsData);
+  if (missionsData.loading) {
+    return (
+      <div>
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  if (missionsData.error) {
+    return (
+      <div>
+        <p>
+          Oops! an error occurred:
+          {missionsData.error}
+        </p>
+      </div>
+    );
+  }
   return (
-    <div>
-      <h1>hello Missions</h1>
+    <div className="container mx-auto px-4">
+      <table className="table-auto w-full border-collapse border">
+        <thead>
+          <tr>
+            <th className="border p-2 pl-0 text-left">Mission</th>
+            <th className="border p-2 text-left">Description</th>
+            <th className="border p-2 text-left">Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {missionsData.missions.map((mission, index) => (
+            <MissionList
+              key={mission.mission_id}
+              mission={mission}
+              index={index}
+            />
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
