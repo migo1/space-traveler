ce2551279b498b989d59dafaa0b25f1b922fabef
olanikegloria github <olanikegloria2020@gmail.com>
2023-07-25 09:36:47 +0100

    created empty profile

---
+++ src/components/missions/Mission.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Mission() {
+    return (
+        <div>
+            <h1>hello Missions</h1>
+        </div>
+    );
+}
+
+export default Mission;
---
+++ src/components/routes.js
@@ -0,0 +1,23 @@
+import Profile from "./profile/Profile";
+import Mission from "./missions/Mission";
+import Rocket from "./rockets/Rocket";
+
+const routes = [
+    {
+        id: 1,
+        path: "/profile",
+        element: <Profile/>
+    },
+    {
+        id: 2,
+        path: "/missions",
+        element: <Mission/>
+    },
+    {
+        id: 3,
+        path: "/rockets",
+        element: <Rocket />
+    }
+]
+
+export default routes;
