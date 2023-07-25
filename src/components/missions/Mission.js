a6059c5873cdf1a1d8531433ce0c7f64e456d3d3
migo1 <ertmigot@gmail.com>
2023-07-25 12:32:45 +0300

    fetch-missions

--- src/components/missions/Mission.js
+++ src/components/missions/Mission.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { getMissions } from '../../redux/missions/missionsSlice';
 
 function Mission() {
+  const missionsData = useSelector((state) => state.missions);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getMissions());
+  }, [dispatch]);
+
+  console.log(missionsData);
   return (
     <div>
       <h1>hello Missions</h1>
--- src/redux/missions/missionsSlice.js
+++ src/redux/missions/missionsSlice.js
@@ -1,4 +1,7 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const baseUrl = 'https://api.spacexdata.com/v3';
 
 const initialState = {
   missions: [],
@@ -6,17 +9,35 @@ const initialState = {
   error: false,
 };
 
+export const getMissions = createAsyncThunk(
+  'spacex/getMissions',
+  async (thunkAPI) => {
+    try {
+      const resp = await axios.get(`${baseUrl}/missions`);
+      return resp.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(`API call error ${e.message}`);
+    }
+  },
+);
+
 const missionsSlice = createSlice({
   name: 'missions',
   initialState,
-  reducers: {
-    getMissions: (state, action) => {
+  reducers: {},
+  extraReducers: {
+    [getMissions.pending]: (state) => {
+      state.loading = true;
+    },
+    [getMissions.fulfilled]: (state, action) => {
       state.missions = action.payload;
       state.loading = false;
     },
-
+    [getMissions.rejected]: (state) => {
+      state.error = true;
+      state.loading = false;
+    },
   },
 });
 
-export const { getMissions, getMissionsFailure } = missionsSlice.actions;
 export default missionsSlice.reducer;
