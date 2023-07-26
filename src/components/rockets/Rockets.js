2d2ae03e5b221e0bbcf004d6bf32db9e1604e66b
olanikegloria github <olanikegloria2020@gmail.com>
2023-07-26 13:40:02 +0100

    implemented bookings

--- src/components/rockets/Rockets.js
+++ src/components/rockets/Rockets.js
@@ -2,9 +2,11 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { getRockets } from '../../redux/rockets/rocketsSlice';
 import Rocket from './Rocket';
+import './rockets.css';
 
 function Rockets() {
   const dispatch = useDispatch();
+
   const rocketsdata = useSelector((state) => state.rockets);
 
   useEffect(() => {
@@ -20,7 +22,7 @@ function Rockets() {
       {rocketsdata.loading ? (
         <div>Loading ...</div>
       ) : (
-        <ul>
+        <ul className="card-container">
           {rocketsdata.rockets.map((rocket) => (
             <Rocket key={rocket.id} rocket={rocket} />
           ))}
